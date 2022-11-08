2860a953ebda2f3baa3d14c03daba47ea0479422
Stas Dubinin <dubinin.stas@gmail.com>
2022-11-08 23:23:19 +0400

    08/11

--- 01-palindrome/script.js
+++ 01-palindrome/script.js
@@ -15,14 +15,30 @@
 
 function palindrome(str) {
     // Напишите код здесь
-}
+    let stroka='';
+    for (let p=0; p<str.length; p++){
+        if(str[p]!==' ' && str[p]!==',' && str[p]!=='!' && str[p]!=='?'){stroka=stroka+str[p]}
+    };
+
+    stroka = stroka.toLowerCase();
+ for (let i=0; i<stroka.length; i+=1){
+   if (i===0){
+    if ((stroka.slice((-1) - i  )) !== stroka[i]){return false};
+   }
+    else {if ((stroka.slice((-1) - i , (-1) - i+1 )) !== stroka[i]){return false}}   
+    }
+    
+    return true;  
+ }
+
+
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(palindrome('топот')); // должно быть true
 console.log(palindrome('Saippuakivikauppias')); // true
 console.log(palindrome('привет')); // false
-
+console.log(palindrome('О, лета тело!')); // true)
 /*
  * Бонус. Задача для любознательных. Пусть функция принимает на вход любую строку,
  * но пробелы и знаки препинания не учитывает. Например:
